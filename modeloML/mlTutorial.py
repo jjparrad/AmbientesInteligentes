977dbae9f377abeafd4e7c185fcaa92a353b2e0d
jose <josemb125@gmail.com>
2020-04-21 08:54:22 -0500

    guardando modelo al ser entrenado y agregando argumentos por comando

--- modeloML/mlTutorial.py
+++ modeloML/mlTutorial.py
@@ -1,92 +1,93 @@
-import sklearn
+import sklearn # modelos de ml
 from sklearn.datasets import load_breast_cancer #db
 from sklearn.model_selection import train_test_split #para dividir en test y train
 from sklearn.naive_bayes import GaussianNB #modelo naives bayes
 from sklearn.metrics import accuracy_score #presicion
-import xlsxwriter
-import xlrd
+import xlsxwriter # para copiar en excel
+import xlrd # para leer en excel
+import argparse # para leer parametros por consola
+import pickle # para guardar y importar el modelo
 
-#load dataset
-data = load_breast_cancer()
+def train_model(excel_file):
+    #loc = ("datos.xlsx") 
+    loc = excel_file
 
-#organize data
-label_names = data['target_names']
-labels = data['target']
-feature_names = data['feature_names']
-features = data['data']
-
-#look data
-# print(label_names)
-# print (len(label_names))
-# print('Class label = ', labels[0])
-# print(feature_names)
-# print(features[0])
-
-workbook = xlsxwriter.Workbook('test.xlsx') # NOMBRE DEL ARCHIVO
-worksheet = workbook.add_worksheet('hoja1') #NOMBRE DEL ARCHIVO
-
-col = 0
-row = 0
-for x in (label_names):
-    worksheet.write(row, col, x)
-    col += 1
-
-col = 0
-row = 1
-for x_var in (labels):
-    worksheet.write(row, col, x_var) 
-    row += 1
-
-col = 2
-row = 0
-for y in (feature_names):
-    worksheet.write(row, col, y)
-    col += 1
-
-col = 2
-row = 1
-z = 0
-while z < len(features):
-    for k in (features[z]):
-        worksheet.write(row, col, k) 
-        col += 1
+    wb = xlrd.open_workbook(loc) 
+    sheet = wb.sheet_by_index(0) 
     
-    col = 2
-    z += 1
-    row += 1
+    sheet.cell_value(0, 0) 
+    
 
-workbook.close()
-
-loc = ("test.xlsx") 
-  
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
-  
-sheet.cell_value(0, 0) 
-  
-print(sheet.row_values(0,2,32))
-label_names = sheet.row_values(0,0,2)
-labels = sheet.col_values(0,1,sheet.nrows)
-feature_names = sheet.row_values(0,2,32)
-i = 1
-features = []
-while i < sheet.nrows:
-    features.append(sheet.row_values(i,2,32))
-    i += 1
+    label_names = sheet.row_values(0,0,1)
+    labels = sheet.col_values(0,1,sheet.nrows)
+    feature_names = sheet.row_values(0,1,6)
+    i = 1
+    features = []
+    while i < sheet.nrows:
+        features.append(sheet.row_values(i,1,6))
+        i += 1
 
 
-#split data
-train, test, train_labels, test_labels = train_test_split(features, labels, test_size = 0.33, random_state = 42)
+    # dividimos los datos, el 66% para entrenamiento y el resto para tests
+    train, test, train_labels, test_labels = train_test_split(features, labels, test_size = 0.33, random_state = 42)
 
-#initialize clasifier# Initialize our classifier
-gnb = GaussianNB()
+    # inicializamos el clasificador ( modelo de ML )
+    gnb = GaussianNB()
 
-# Train our classifier
-model = gnb.fit(train, train_labels)
+    # entrenamos el clasificador
+    print("entrenando modelo...")
+    model = gnb.fit(train, train_labels)
 
-# Make predictions
-preds = gnb.predict(test)
-print(preds)
+    # guardamos el clasificador
+    filename = 'modelo_emociones_eafit.sav'
+    pickle.dump(model, open(filename, 'wb'))
+    print("modelo guardado como < modelo_emociones_eafit.sav >")
 
-# Evaluate accuracy
-print("precicion de = ", accuracy_score(test_labels, preds))
+    # hacemos predicciones con los datos de test
+    preds = gnb.predict(test)
+    print(preds)
+
+    # Evaluamos la precision comparando el test con las predicciones sacadas.
+    print("precision de = ", accuracy_score(test_labels, preds))
+
+
+def predecir(excel_file):
+    # cargamos los datos de excel
+    loc = excel_file
+    wb = xlrd.open_workbook(loc) 
+    sheet = wb.sheet_by_index(0) 
+    sheet.cell_value(0, 0)
+
+    # organizamos los datos para ser enviados al modelo
+    i = 1
+    features = []
+    while i < sheet.nrows:
+        features.append(sheet.row_values(i,1,6))
+        i += 1
+
+    # cargamos el modelo
+    filename = 'modelo_emociones_eafit.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    # hacemos la prediccion
+    result = loaded_model.predict(features)
+    print(result)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-train", help="train the algorithm with a <dataset>")
+    group.add_argument("-predict", help="make a prediction with a <excel file>")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    
+    if args.train:
+        print("entrenaremos el modelo con la base de datos:", args.train)
+        train_model(args.train)
+    elif args.predict:
+        print("haremos una prediccion los los valores de:", args.predict)
+        predecir(args.predict)
+    else:
+        print("sin argumentos")
